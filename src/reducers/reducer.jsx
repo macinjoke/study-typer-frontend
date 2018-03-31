336377037d6d389763e0e8f756d6fd29741f3e44
Shunji Makino <shunji.makino@gmail.com>
2018-03-31 15:40:34 +0900

    refactor from `Object.assign` to Destructing assignment

--- src/reducers/reducer.jsx
+++ src/reducers/reducer.jsx
@@ -21,35 +21,38 @@ const initialState = {
 const reducer = (state: State=initialState, action: any) => {
   switch (action.type) {
     case 'INPUT_KEY': {
-      return Object.assign({}, state, {
+      return {
+        ...state,
         pushedKey: action.value,
         currentInput: state.currentInput + action.value,
-      })
+      }
     }
     case 'CLEAR_INPUT': {
-      return Object.assign({}, state, {currentInput: '', matchingIndex: -1})
+      return {...state, currentInput: '', matchingIndex: -1}
     }
     case 'LOAD_WORDS': {
-      return Object.assign({}, state, {words: action.words})
+      return {...state, words: action.words}
     }
     case 'SET_WORD': {
-      return Object.assign({}, state, {wordIndex: action.index})
+      return {...state, wordIndex: action.index}
     }
     case 'BACK_CHAR': {
-      return Object.assign({}, state, {
-        currentInput: state.currentInput.slice(0, -1)
-      })
+      return {...state, currentInput: state.currentInput.slice(0, -1)}
     }
     case 'SET_MATCHING_INDEX': {
-      return Object.assign({}, state, {matchingIndex: action.index})
+      return {...state, matchingIndex: action.index}
     }
     case 'SET_RANK': {
-      return Object.assign({}, state, {rank: action.value})
+      return {...state, rank: action.value}
     }
     case 'RESET': {
-      return Object.assign({}, state, {
-        pushedKey: '', wordIndex: 0, matchingIndex: -1, currentInput: ''
-      })
+      return {
+        ...state,
+        pushedKey: '',
+        wordIndex: 0,
+        matchingIndex: -1,
+        currentInput: ''
+      }
     }
     default: {
       return state;
