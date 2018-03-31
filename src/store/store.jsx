c9f73b8b71b47655ea681fd495fd285fa96c0483
Shunji Makino <shunji.makino@gmail.com>
2018-04-01 00:35:24 +0900

    add setting for redux-devtools-extension

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,5 +3,8 @@ import { createStore } from 'redux';
 import reducer from '../reducers/reducer';
 
 export default function configureStore() {
-  return createStore(reducer);
+  return createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
