e8f56f80af16238af485c8512a547c2e14ae6dfd
Shunji Makino <shunji.makino@gmail.com>
2018-04-05 19:27:16 +0900

    introduce redux-thunk

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,14 @@
 // @flow
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/reducer';
+import thunk from 'redux-thunk';
 
 import type {Store} from '../types'
 
 export default function configureStore(): Store {
   return createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk),
   );
 }
