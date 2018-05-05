f13142f35af20fc7e067283723100038b3644545
Shunji Makino <shunji.makino@gmail.com>
2018-05-05 16:22:41 +0900

    fix `webpack.config.js` format (eslint --fix)

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var path = require("path");
-var config = require("config");
+const path = require('path')
+const config = require('config')
 
 module.exports = {
   entry: {
-    app: ["babel-polyfill", "./src/index.jsx"]
+    app: ['babel-polyfill', './src/index.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/assets/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: 'bundle.js',
   },
   devtool: 'source-map',
   module: {
     rules: [
       {
         test: /.jsx?$/,
-        use: "babel-loader",
-        exclude: /node_modules/
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"]
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     host: config.dev_server.host,
     port: config.dev_server.port,
-    disableHostCheck: true
+    disableHostCheck: true,
   },
   externals: {
-    config: JSON.stringify(config.frontend)
-  }
-};
+    config: JSON.stringify(config.frontend),
+  },
+}
