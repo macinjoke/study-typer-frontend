25b2b2328a38c9c4e8b867a28d5fec306c446d0d
Shunji Makino <shunji.makino@gmail.com>
2018-07-18 19:22:46 +0900

    add `音が出ます` warning

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,9 @@ const Header = () => (
       </div>
       <p id="subtitle">Typing game for English study</p>
     </div>
+    <div id="warning-panel">
+      <p>音が出ます</p>
+    </div>
     <div id="explanation-panel" className="text-white font-weight-light">
       <p>英語勉強のためのタイピングゲームです</p>
       <p>日本語訳を確認しつつ表示される英単語を打ち込みましょう</p>
