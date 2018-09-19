fd06f6405ddf0a3f9184010eb58dc0176f15aff2
Shunji Makino <shunji.makino@gmail.com>
2018-09-19 11:36:24 +0900

    reducer のテスト内でactionCreatorを利用する

--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -1,12 +1,12 @@
 import reducer, { initialState } from './reducer'
+import * as actions from '../actions/actions'
 
 test('INPUT_KEY action でcurrentInput に1文字追加する', () => {
   const beforeState = {
     ...initialState,
     currentInput: 'hoge',
   }
-  const action = { type: 'INPUT_KEY', value: 'j' }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.inputKey('j'))
   expect(afterState).toEqual({ ...beforeState, currentInput: 'hogej' })
 })
 
@@ -15,15 +15,13 @@ test('CLEAR_INPUT action でcurrentInput が空文字になる', () => {
     ...initialState,
     currentInput: 'hoge',
   }
-  const action = { type: 'CLEAR_INPUT' }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.clearInput())
   expect(afterState).toEqual({ ...beforeState, currentInput: '' })
 })
 
 test('SET_WORD action でwordIndex の値が変わる', () => {
   const beforeState = initialState
-  const action = { type: 'SET_WORD', index: 3 }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.setWord(3))
   expect(afterState).toEqual({ ...beforeState, wordIndex: 3 })
 })
 
@@ -32,22 +30,19 @@ test('BACK_CHAR action でcurrentInput が1文字減る', () => {
     ...initialState,
     currentInput: 'hoge',
   }
-  const action = { type: 'BACK_CHAR' }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.backChar())
   expect(afterState).toEqual({ ...beforeState, currentInput: 'hog' })
 })
 
 test('SET_RANK action でrank の値が変わる', () => {
   const beforeState = initialState
-  const action = { type: 'SET_RANK', value: 3 }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.setRank(3))
   expect(afterState).toEqual({ ...beforeState, rank: 3 })
 })
 
 test('FETCH_WORDS_REQUEST action でisFetching がtrueになる', () => {
   const beforeState = initialState
-  const action = { type: 'FETCH_WORDS_REQUEST' }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.fetchWordsRequest())
   expect(afterState).toEqual({ ...beforeState, isFetching: true })
 })
 
@@ -71,11 +66,7 @@ test('FETCH_WORDS_SUCCESS action', () => {
       rank: 3,
     },
   ]
-  const action = {
-    type: 'FETCH_WORDS_SUCCESS',
-    words: newWords,
-  }
-  const afterState = reducer(beforeState, action)
+  const afterState = reducer(beforeState, actions.fetchWordsSuccess(newWords))
   expect(afterState).toEqual({
     ...beforeState,
     isFetching: false,
@@ -87,9 +78,8 @@ test('FETCH_WORDS_SUCCESS action', () => {
 
 test('FETCH_WORDS_ERROR action', () => {
   const beforeState = { ...initialState, isFetching: true }
-  const err = { errorMsg: 'hoge' }
-  const action = { type: 'FETCH_WORDS_ERROR', err }
-  const afterState = reducer(beforeState, action)
+  const err = { status: 503 }
+  const afterState = reducer(beforeState, actions.fetchWordsError(err))
   expect(afterState).toEqual({
     ...beforeState,
     isFetching: false,
